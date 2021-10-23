93b72f3cd473f33ac9c0fc9a055d930b8bb77185
yasmin <yasmin.abdellh@gmail.com>
2021-10-23 02:27:12 +0200

    day3 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'myfirstapp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+  
+  
 export class AppComponent {
   title = 'lecture2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProductsComponent } from './component/products/products.component';
+import { HighlighterDirective } from './Directives/highlighter.directive';
+import { Product2Component } from './component/product2/product2.component';
+import { USDtoEGPPipe } from './Pipe/usdto-egp.pipe';
+import { CartparentComponent } from './sharingDate/cartparent/cartparent.component';
+import { CartchildComponent } from './sharingDate/cartchild/cartchild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    HighlighterDirective,
+    Product2Component,
+    USDtoEGPPipe,
+    CartparentComponent,
+    CartchildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
